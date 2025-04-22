05f11e78bd2dd18b516bbdbd7528bf366f79d609
Milo <milovocker.clase@gmail.com>
2025-04-22 15:14:24 +0100

    añadida ruta para pagina detalle de contenido

--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,8 @@ Route::middleware('auth')->group(function () {
 Route::get('/{tipo}', [ContenidoController::class, 'abrirPagina'])->name('contenido');
 Route::get('/peliculas-por-genero', [ContenidoController::class, 'obtenerPeliculasPorGenero']);
 
+Route::get('/detalles/{id}', [ContenidoController::class, 'abrirPaginaDetalle']);
+
+
+
 require __DIR__.'/auth.php';
