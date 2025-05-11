41dd1e848b21b97e1392bbdb233856afc5dfbd3e
aleixbermudez <aleixbermudez19@gmail.com>
2025-05-11 18:47:56 +0100

    Vite

--- vite.config.js
+++ vite.config.js
@@ -7,12 +7,5 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-    ],
-    build: {
-        manifest: true,
-        outDir: 'public/build',
-        rollupOptions: {
-            input: 'resources/js/app.js',
-        },
-    },
+    ]
 });
